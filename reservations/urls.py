219fa149198dbf5fa0eada6e693b2cdba41faab5
IraDahiya <iradahiyaa22@gmail.com>
2025-06-23 04:23:51 +0530

    Fix: redirect root to reservations/home

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.shortcuts import redirect  # 👈 Add this line
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', include('reservations.urls')),
-    path('', RedirectView.as_view(url='/reservations/', permanent=True)),  # Redirect root '/' to '/reservations/'
+
+    # 👇 Redirect root URL to /reservations/
+    path('', lambda request: redirect('home')),
 ]
